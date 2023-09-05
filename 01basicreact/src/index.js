6add307e6ad5f053dfaee5e8928396194c7ef100
Sainath-Kale <sainathkale51660@gmail.com>
2023-09-06 00:50:02 +0530

    custom react added

--- 01basicreact/src/index.js
+++ 01basicreact/src/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+/*
+function MyApp(){
+  return(
+    <h1>Custom React</h1>
+  )
+}
+*/
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com',target: '_blank' },
+  'click me to visit google',
+  anotherUser
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <App/>
+  </StrictMode>
+   
 );
 
